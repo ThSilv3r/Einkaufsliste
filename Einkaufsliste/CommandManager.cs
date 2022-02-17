01ca1aac55b71966ed1833343bfe3df0e6c50dc3
Jonas <lppoweryoshi@gmail.com>
2022-02-17 11:05:37 +0100

    Added Commands

---
+++ Einkaufsliste/CommandManager.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Einkaufsliste
+{
+    public class CommandManager
+    {
+        public void foodCommands(string[] args)
+        {
+            FoodManager foodManager = new FoodManager();
+            if (args[0] == "createFood")
+            {
+                foodManager.createFood();
+            }
+            else if(args[0] == "deleteFood")
+            {
+                foodManager.deleteFood(args[1]);
+            }
+            else if (args[0] == "getFoodList")
+            {
+                foodManager.getFoodList();
+            }
+            else
+            {
+                Console.WriteLine("Kein echter Befehl");
+            }
+        }
+        public void listCommands(string[] args)
+        {
+            ShoppingListManager listManager = new ShoppingListManager();
+            if (args[0] == "createShoppingList")
+            {
+                Console.WriteLine("Creating ShoppingList");
+                listManager.createShoppingList();
+            }
+            else if (args[0] == "getShoppingList")
+            {
+                listManager.GetShoppingList(args[1]);
+            }
+            else if (args[0] == "deleteShoppingList")
+            {
+                listManager.deleteShoppingList(args[1]);
+            }
+            else if (args[0] == "addProductToShoppingList")
+            {
+                listManager.addProduct(args[1]);
+            }
+            else if (args[0] == "addFoodToShoppingList")
+            {
+                listManager.addFood(args[1]);
+            }
+            else
+            {
+                Console.WriteLine("Kein echter Befehl");
+            }
+        }
+        public void productCommand(string[] args)
+        {
+            ProductManager productManager = new ProductManager();
+            if (args[0] == "createProduct")
+            {
+                productManager.createProduct();
+            }
+            else if (args[0] == "deleteProduct")
+            {
+                productManager.deleteProduct(args[1]);
+            }
+            else if (args[0] == "getProductList")
+            {
+                productManager.getProductList();
+            }
+            else
+            {
+                Console.WriteLine("Kein echter Befehl");
+            }
+        }
+        public void recipeCommand(string[] args)
+        {
+            RecipeManager recipeManager = new RecipeManager();
+            if (args[0] == "createRecipe")
+            {
+                recipeManager.createRecipe();
+
+            }
+            else if (args[0] == "GetRecipe")
+            {
+                recipeManager.GetRecipe(args[1]);
+            }
+            else if (args[0] == "deleteRecipe")
+            {
+                recipeManager.deleteRecipe(args[1]);
+            }
+            else if (args[0] == "addRecipeToList")
+            {
+                recipeManager.addToShoppingList();
+            }
+            else
+            {
+                Console.WriteLine("Kein echter Befehl");
+            }
+        }
+    }
+}
--- Einkaufsliste/Program.cs
+++ Einkaufsliste/Program.cs
@@ -6,7 +6,36 @@ namespace Einkaufsliste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandManager commandManager = new CommandManager();
+            if (args.Length <= 0) //Checking the Length helps avoid NullReferenceException at args[0]
+            {
+                Console.WriteLine("Please enter a command");
+            }
+            else
+            {
+                if (args[0].Contains("Food"))
+                {
+                    commandManager.foodCommands(args);
+                }
+                if (args[0].Contains("Product"))
+                {
+                    commandManager.productCommand(args);
+                }
+                if (args[0].Contains("Recipe"))
+                {
+                    commandManager.recipeCommand(args);
+                }
+                if (args[0].Contains("ShoppingList"))
+                {
+                    commandManager.listCommands(args);
+                }
+                else
+                {
+                    Console.WriteLine(args[0]);
+                    Console.WriteLine("Kein echter Befehl");
+                    Console.WriteLine(args.Length);
+                }
+            }
         }
     }
 }
